983f9f35004f5b253be629202613157bb33054f6
Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>
2015-04-01 15:55:22 -0400

    Initial commit.

---
+++ src/cst1201/DieRoller.java
@@ -0,0 +1,16 @@
+package cst1201;
+
+public class DieRoller {
+
+    public static void main(String[] args) {
+        //TODO: Create a Scanner object to read keyboard input.
+        
+        //TODO: Ask the user a lower and upper bound.
+        
+        //TODO: Ask the user for the number of random integers to generate.
+        
+        //TODO: Generate random integers of the desired length and bound.
+        
+        //TODO: Print the average, max, min, count, and sum of the integers.
+    }
+}
