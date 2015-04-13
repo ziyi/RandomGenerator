4e0b11e53e85c78e98dfea43773da74231e70ff2
G24 <G24@G600-PC24.academic.citytech.cuny.edu>
2015-04-13 15:25:44 -0400

    Hw : RandomGenerator

--- src/cst1201/RandomGenerator.java
+++ src/cst1201/RandomGenerator.java
@@ -1,16 +1,41 @@
+//author Ziying Guo
 package cst1201;
-
+import java.util.Scanner;
+import java.util.Random; 
 public class RandomGenerator {
 
     public static void main(String[] args) {
-        //TODO: Create a Scanner object to read keyboard input.
+         int sum = 0;
+         int maxx =0;
+         int minn;
+         System.out.print("Input a number of random integers to generate: ");
+         Scanner keyboard = new Scanner(System.in);
         
-        //TODO: Ask the user a lower and upper bound.
+         int num = keyboard.nextInt();
+         Random rand = new Random();
+         System.out.print("Input a max number:");
+         int max = keyboard.nextInt();
+         System.out.print("Input a min number:");
+         int min = keyboard.nextInt();
+         minn = min + 1;
+       
+         System.out.print("The random numbers are: ");
+         for (int i=0;i<num;i++)
+         {int randomNum = rand.nextInt((max - min) + 1) + min;
+         System.out.print(randomNum + "   ");
+         sum = sum + randomNum;
+         if (maxx<randomNum)
+         {maxx=randomNum;}
+         if (minn>randomNum)
+         {minn= randomNum;}
+         }
+         System.out.println();
         
-        //TODO: Ask the user for the number of random integers to generate.
-        
-        //TODO: Generate random integers of the desired length and bound.
-        
-        //TODO: Print the average, max, min, count, and sum of the integers.
+         int average = sum / 5;
+         System.out.println("The average is " + average);
+         System.out.println("The sum is " + sum);
+         System.out.println("The max number is " + maxx);
+         System.out.println("The min number is " + minn);
+         
     }
 }
